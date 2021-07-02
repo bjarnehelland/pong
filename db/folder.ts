21da88a20e606f1a7855ef69eee28797d843f472
Bjarne Helland <bjarneh@stacc.com>
2021-07-02 13:45:18 +0200

    add files

---
+++ db/folder.ts
@@ -0,0 +1,7 @@
+
+import { Db } from 'mongodb'
+import { nanoid } from 'nanoid'
+
+export const createFolder = async (db: Db, folder: { createdBy: string; name: string }) => {}
+
+export const getFolders = async (db: Db, userId: string) => {}
